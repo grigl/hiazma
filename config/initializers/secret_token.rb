4670f009113a7e97e8ac271f904ebf5a32e4abf3
Gleb Grishin <griglmail@gmail.com>
2012-07-04 16:13:19 +0400

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiazma::Application.config.secret_token = '024fac745f96e57a0e72378f6b1b63b15945538d3099cf7c8a9c1454d8ea9c9107cad3f5b4d8cc1473727ae8ff6240698e81c8e84c9c2842abadf8f5fe4456e3'
