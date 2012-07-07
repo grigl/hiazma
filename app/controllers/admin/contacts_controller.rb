7211f84536e7272c463ccc60580def31bb64f047
Gleb Grishin <griglmail@gmail.com>
2012-07-07 14:57:52 +0400

    admin contacts view and controller

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,7 +1,15 @@
 class Admin::ContactsController < Admin::AdminController
   def show
+    @contact = Contact.first
   end
 
   def update
+    @contact = Contact.find(params[:format])
+
+    if @contact.update_attributes(params[:contact])
+      redirect_to :back, notice: 'contacts updated'
+    else
+      redirect_to :back, alert: 'errors'
+    end
   end
 end
