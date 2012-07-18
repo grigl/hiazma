884d21c63b745929e2eb0ec677759355a586d597
Gleb Grishin <griglmail@gmail.com>
2012-07-18 17:14:52 +0400

    cleaning up #1

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,8 @@ class Article < ActiveRecord::Base
   attr_accessible :description, :image, :title
   mount_uploader :image, ImageUploader
 
+  translates :title, :description
+  accepts_nested_attributes_for :translations
+
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120705115628) do
+ActiveRecord::Schema.define(:version => 20120718032101) do
 
   create_table "abouts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(:version => 20120705115628) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "article_translations", :force => true do |t|
+    t.integer  "article_id"
+    t.string   "locale"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "article_translations", ["article_id"], :name => "index_article_translations_on_article_id"
+  add_index "article_translations", ["locale"], :name => "index_article_translations_on_locale"
 
   create_table "articles", :force => true do |t|
     t.string   "title"
