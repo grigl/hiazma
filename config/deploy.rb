c08422e65096d37553d8fc5cbcfebb4e0a64406a
Evgeniy Kirov <evg.kirov@gmail.com>
2012-07-24 19:24:13 +0400

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,24 +12,33 @@ server "hiazma.com", :app, :web, :db, :primary => true
 set :user, "rails"
 set :deploy_to, "/home/rails/apps/hiazma"
 
+set :unicorn_binary, "unicorn"
+set(:unicorn_pid) {"#{current_path}/tmp/pids/unicorn.pid"}
+set :unicorn_config, "/home/rails/unicorns/hiazma.rb"
+after 'deploy:restart', 'unicorn:restart'
+
 after "deploy:setup" do
   run "mkdir -p #{deploy_to}/shared/pids && mkdir -p #{deploy_to}/shared/config && mkdir -p #{deploy_to}/shared/var"
 end
 
 namespace :unicorn do
-  task :start do
-    run "cd #{deploy_to}/current && unicorn_rails -c #{deploy_to}/current/config/unicorn.rb -e #{rails_env} -D"
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "#{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D #{current_path}/config.ru"
   end
-
-  task :stop do
-    run "kill -9 `cat #{deploy_to}/shared/pids/unicorn.pid`"
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "kill `cat #{unicorn_pid}`"
   end
-
-  task :restart do
-    run "kill -HUP `cat #{deploy_to}/shared/pids/unicorn.pid`"
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "kill `cat #{unicorn_pid}` && true"
+    start
   end
 end
-
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
