4583f6546b4282745e9cec237339522e449b1826
Evgeniy Kirov <evg.kirov@gmail.com>
2012-07-24 23:26:32 +0400

    Надо сделать так, что если у контента нет языковой версии, то она не отображается (чтобы иногда для разных языков разное показывать). https://basecamp.com/1748678/projects/669953-hiazma-games/todos/9416407#comment_16454808

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Hiazma
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.fallbacks = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -70,7 +71,7 @@ module Hiazma
     config.assets.version = '1.0'
 
     config.to_prepare do
-      Devise::SessionsController.layout 'admin' 
+      Devise::SessionsController.layout 'admin'
     end
   end
 end
