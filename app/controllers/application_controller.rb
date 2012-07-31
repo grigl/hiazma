56c546a63771f02391328615bb1d645a735f0544
Evgeniy Kirov <evg.kirov@gmail.com>
2012-07-31 04:41:25 +0400

    Автоматическое определение языка https://basecamp.com/1748678/projects/669953-hiazma-games/todos/11685461

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,22 @@ class ApplicationController < ActionController::Base
   layout 'application'
 
   before_filter :set_locale
- 
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    detected = extract_locale_from_accept_language_header
+    if not I18n.available_locales.include? detected.to_sym
+      detected = I18n.default_locale
+    end
+    I18n.locale = params[:locale] || detected
   end
 
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
