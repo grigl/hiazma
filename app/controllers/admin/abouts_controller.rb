dcda8869fe4374e89b8fbcdcef85e1a83212f8e1
Gray Hound <admin@devilmaydie.name>
2012-08-23 21:46:39 +0600

    фиксаем about

--- app/controllers/admin/abouts_controller.rb
+++ app/controllers/admin/abouts_controller.rb
@@ -5,8 +5,10 @@ class Admin::AboutsController < Admin::AdminController
 
   def update
     @about = About.find(params[:about][:id])
+    @about.title = params[:about][:title]
+    @about.description = params[:about][:description]
 
-    if @about.update_attributes(params[:about].except(:id))
+    if @about.save
       redirect_to :back, notice: "about updated"
     else
       @errors = @about.errors.full_messages.join(' ')
